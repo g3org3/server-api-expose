d127323dace188fdd8d81a20a1bf0cf70c70c071
George <7jagjag@gmail.com>
2018-08-04 08:16:48 +0200

    nginx api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const execa = require('execa');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/api/nginx/conf', (req, res) => res.send('Hello World!'));
+app.get('/api/nginx/reload', (req, res) =>
+  execa('nginx -s reload')
+    .then(({ stdout }) => res.json({ stdout }))
+    .catch(err => res.json(err.message)),
+);
+app.get('/api/nginx/stop', (req, res) => {
+  execa('nginx -s stop')
+    .then(({ stdout }) => res.json({ stdout }))
+    .catch(err => res.json(err.message));
+});
+app.get('/api/nginx/start', (req, res) =>
+  execa('nginx')
+    .then(({ stdout }) => res.json({ stdout }))
+    .catch(err => res.json(err.message)),
+);
 app.listen(9876, () => console.log('Example app listening on port 3000!'));
